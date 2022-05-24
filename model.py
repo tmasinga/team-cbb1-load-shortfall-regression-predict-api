235bd68d0c155910238555dc8c3e7cc239a6ffba
tmasinga <tinyikomasinga20@gmail.com>
2022-05-24 18:35:52 +0800

    file changes

--- model.py
+++ model.py
@@ -58,7 +58,14 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    predict_vector = feature_vector_df[[ 
+    'Madrid_wind_speed','Valencia_wind_speed','Bilbao_wind_speed', 
+    'Barcelona_wind_speed','Seville_wind_speed','Barcelona_rain_1h',
+    'Seville_rain_1h','Bilbao_snow_3h','Seville_rain_3h','Madrid_rain_1h', 
+    'Barcelona_rain_3h', 'Valencia_snow_3h','Seville_temp_max','Valencia_temp_max', 
+    'Valencia_temp','Seville_temp','Valencia_temp_min', 'Barcelona_temp_max', 
+    'Madrid_temp_max','Barcelona_temp', 'Bilbao_temp_min', 'Bilbao_temp',
+    'Barcelona_temp_min', 'Bilbao_temp_max', 'Seville_temp_min','Madrid_temp', 'Madrid_temp_min']]
     # ------------------------------------------------------------------------
 
     return predict_vector
